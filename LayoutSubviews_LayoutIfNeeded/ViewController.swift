a3134db476cdc5a8fb940b91097e7576e08d1a02
komplektan <komplektan@mail.ru>
2022-07-02 14:02:31 +0500

    Start project

--- LayoutSubviews_LayoutIfNeeded/ViewController.swift
+++ LayoutSubviews_LayoutIfNeeded/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewHA: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewHA.layer.cornerRadius = viewHA.frame.height / 2
     }
 
-
+    @IBAction func Button(_ sender: Any) {
+        
+    }
 }
 
